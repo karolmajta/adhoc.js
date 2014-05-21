bb4d206f345c2aad36a53b81c847cfb491965e41
Karol Majta <karol@karolmajta.com>
2014-05-21 17:18:43 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            source: {
+                files: ['Gruntfile.js', 'src/**/*.js'],
+                tasks: ['jshint:all', 'copy']
+            }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js']
+        },
+        copy: {
+            exampleLibs: {
+                files: [
+                    {'examples/libs/q.js': ['bower_components/q/q.js']},
+                    {'examples/libs/request.js': ['bower_components/browser-request/dist/browser/request.js']},
+                    {'examples/libs/underscore.js': ['bower_components/underscore/underscore.js']},
+                    {'examples/libs/adhoc.js': ['src/adhoc.js']}
+                ]
+            }
+        },
+        'http-server': {
+            dev: {
+                root: "examples/",
+                port: 8282,
+                host: "127.0.0.1",
+                cache: 0,
+                showDir: true,
+                autoIndex: true,
+                defaultExt: "html",
+                runInBackground: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-http-server');
+
+    grunt.registerTask('default', ['copy', 'http-server:dev', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+adhoc.js
+========
+
+Parallel computations library, that can take opportunistic advantage of
+ad hoc clouds.
